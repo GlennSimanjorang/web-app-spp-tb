fa0fb788995d120f3f23beefe74c7bef187e73a8
Ucokkks <glenmartua@gmail.com>
2025-11-28 10:48:03 +0700

    fix: migrate user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -8,14 +8,16 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('id')->primary();
-            $table->string('name');
-            $table->enum('role', ['admin', 'parents']);
-            $table->string('email')->unique();
-            $table->text('password');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('number');
+                $table->enum('role', ['admin', 'parents'])->default('parents'); // perhatikan typo "user" → seharusnya "parents"
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
